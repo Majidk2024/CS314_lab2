2a74a275e951a124245039a6be4fad11b32fde32
Majidk2024 <cs24dp006@iitdh.ac.in>
2024-08-12 17:52:55 +0530

    Update part3_searcher.cpp

--- part3_searcher.cpp
+++ part3_searcher.cpp
@@ -6,59 +6,49 @@
 
 using namespace std;
 
-int main(int argc, char **argv)
-{
-	if(argc != 5)
-	{
-		// cout <<"usage: ./partitioner.out <path-to-file> <pattern> <search-start-position> <search-end-position>\nprovided arguments:\n";
-		for(int i = 0; i < argc; i++)
-			cout << argv[i] << "\n";
-		return -1;
-	}
-	
-	char *file_to_search_in = argv[1];
-	char *pattern_to_search_for = argv[2];
-	int search_start_position = atoi(argv[3]);
-	int search_end_position = atoi(argv[4]);
+void signal_parent() {
+    pid_t parent_pid = getppid(); // Get parent process ID
+    if (parent_pid > 1) { // Parent exists
+        cout << "[" << getpid() << "] signaling parent " << parent_pid << endl;
+        kill(parent_pid, SIGTERM); // Signal parent to terminate
+    }
+}
 
-	//TODO
-	ifstream file(file_to_search_in, ios::in | ios::binary);
-		if(!file.is_open()){
-			cerr<<"unable to open file: "<< *file_to_search_in << endl;
-			return false;
-		}
-		else{
-			// now we will read the file from 0 Bytes to 67108863 Bytes 
-			// for that we will calculate the length to read from file.txt
-			int length_to_read = search_end_position - search_start_position + 1;
-			// now to store the file portion to read we need certain storage
-			char * buffer = new char[length_to_read+1];
-			//we will point to the start of file
-			file.seekg(search_start_position, ios::beg);
-			// now we will read the file upto to length_to_read
-			file.read(buffer, length_to_read);
-			// we need to end the buffer with \0
-			buffer[length_to_read + 1] = '\0';
-			// now since we read binary content need to conver to string 
-			string content(buffer);
-			//deleting the buffer and closing file
-			delete [] buffer;
-			file.close();
-			//now to find the substring 
-			size_t found_pos = content.find(string(pattern_to_search_for));
-			if(found_pos != string::npos){
-				int process = getpid();
-				cout << "[" << process << "] found at " << (search_start_position + found_pos) << endl;
-				//now since we got the pattern we are searching we can stop the search by killing all the other and all the other processes
-				//now we just need to call kill(0)
-				kill(0, SIGKILL);
-			}
-			else{
-				cout << "[" << getpid() << "] didn't find" << endl;
+int main(int argc, char **argv) {
+    if(argc != 5) {
+        for(int i = 0; i < argc; i++)
+            cout << argv[i] << "\n";
+        return -1;
+    }
     
-			}
-		}
-	// cout << "[-1] didn't find\n";
-	// return 0;
-	return 0;
+    char *file_to_search_in = argv[1];
+    char *pattern_to_search_for = argv[2];
+    int search_start_position = atoi(argv[3]);
+    int search_end_position = atoi(argv[4]);
+
+    ifstream file(file_to_search_in, ios::in | ios::binary);
+    if(!file.is_open()) {
+        cerr << "Unable to open file: " << file_to_search_in << endl;
+        return 1;
+    }
+
+    int length_to_read = search_end_position - search_start_position + 1;
+    char *buffer = new char[length_to_read + 1];
+    file.seekg(search_start_position, ios::beg);
+    file.read(buffer, length_to_read);
+    buffer[length_to_read] = '\0';
+    string content(buffer);
+    delete[] buffer;
+    file.close();
+
+    size_t found_pos = content.find(string(pattern_to_search_for));
+    if(found_pos != string::npos) {
+        int process = getpid();
+        cout << "[" << process << "] found at " << (search_start_position + found_pos) << endl;
+        signal_parent(); // Signal parent to terminate
+        return 1;
+    } else {
+        cout << "[" << getpid() << "] didn't find" << endl;
+        return 0;
+    }
 }
